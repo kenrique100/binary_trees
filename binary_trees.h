e7194ab48e8fe27f78baff4e717788851b1ba1c3
Kenrique <ngwakneri2016@gmail.com>
2023-11-28 21:22:39 +0100

    Please readme

--- binary_trees.h
+++ binary_trees.h
@@ -63,4 +63,9 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 /*
 * int binary_tree_is_complete(const binary_tree_t *tree);
 */
+
+/* Node Relationships */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 #endif
